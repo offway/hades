9d377f30c4e3c46da1bd8f929c626b6686dca1a5
XiaZhengXin <xzx@xiazhengxin.name>
2019-04-08 16:04:19 +0800

    明星同款 增加页面 ok...
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/hades/controller/StarSameController.java
+++ src/main/java/cn/offway/hades/controller/StarSameController.java
@@ -1,32 +1,47 @@
 package cn.offway.hades.controller;
 
+import cn.offway.hades.domain.PhGoods;
 import cn.offway.hades.domain.PhStarsame;
+import cn.offway.hades.domain.PhStarsameGoods;
+import cn.offway.hades.domain.PhStarsameImage;
 import cn.offway.hades.properties.QiniuProperties;
+import cn.offway.hades.service.PhGoodsService;
 import cn.offway.hades.service.PhStarsameGoodsService;
 import cn.offway.hades.service.PhStarsameImageService;
 import cn.offway.hades.service.PhStarsameService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.qiniu.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequestMapping
 public class StarSameController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private PhStarsameService starsameService;
     @Autowired
     private PhStarsameGoodsService starsameGoodsService;
     @Autowired
     private PhStarsameImageService starsameImageService;
+    @Autowired
+    private PhGoodsService goodsService;
     @Autowired
     private QiniuProperties qiniuProperties;
 
@@ -64,8 +79,46 @@ public class StarSameController {
     }
 
     @ResponseBody
-    @RequestMapping("/starSame_save")
-    public void save(PhStarsame starsame, String goodsIds, String images) {
-        starsameService.save(starsame);
+    @PostMapping("/starSame_save")
+    public boolean save(PhStarsame starsame, String goodsID, String imagesJSONStr) {
+        PhStarsame starsameObj = starsameService.save(starsame);
+        String[] goodsList = goodsID.split(",");
+        for (String gid : goodsList) {
+            PhGoods goods = goodsService.findOne(Long.valueOf(gid));
+            if (goods != null) {
+                PhStarsameGoods starsameGoods = new PhStarsameGoods();
+                starsameGoods.setStarsameId(starsameObj.getId());
+                starsameGoods.setStarsameTitle(starsameObj.getTitle());
+                starsameGoods.setGoodsId(goods.getId());
+                starsameGoods.setGoodsName(goods.getName());
+                starsameGoods.setGoodsImage(goods.getImage());
+                starsameGoods.setBrandId(goods.getBrandId());
+                starsameGoods.setBrandName(goods.getBrandName());
+                starsameGoods.setBrandLogo(goods.getBrandLogo());
+                starsameGoods.setRemark(goods.getRemark());
+                starsameGoods.setCreateTime(new Date());
+                starsameGoodsService.save(starsameGoods);
+            } else {
+                logger.error("goods Id 非法");
+            }
+        }
+        String text = new String(Base64.decode(imagesJSONStr.getBytes(), Base64.DEFAULT));
+        JSONArray jsonArray = JSON.parseArray(text);
+        if (jsonArray != null) {
+            for (Object o : jsonArray) {
+                JSONObject jsonObject = (JSONObject) o;
+                PhStarsameImage starsameImage = new PhStarsameImage();
+                starsameImage.setStarsameId(starsameObj.getId());
+                starsameImage.setStarsameTitle(starsameObj.getTitle());
+                starsameImage.setImageUrl(jsonObject.getString("url"));
+                starsameImage.setRemark(jsonObject.getString("remark"));
+                starsameImage.setSort(jsonObject.getLong("sort"));
+                starsameImage.setCreateTime(new Date());
+                starsameImageService.save(starsameImage);
+            }
+        } else {
+            logger.error("images json 非法");
+        }
+        return true;
     }
 }
