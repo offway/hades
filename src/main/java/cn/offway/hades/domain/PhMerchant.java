0d7b8c4bf21f075dd33924a86b0978e62f734763
weinan <weinan@offway-art.com>
2019-04-12 17:59:28 +0800

    添加运费模板

--- src/main/java/cn/offway/hades/domain/PhMerchant.java
+++ src/main/java/cn/offway/hades/domain/PhMerchant.java
@@ -1,7 +1,5 @@
 package cn.offway.hades.domain;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.io.Serializable;
 import javax.persistence.*;
 import java.util.Date;
@@ -41,22 +39,31 @@ public class PhMerchant implements Serializable {
     private String contractNo;
 
     /** 签约日期 **/
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date signDate;
 
     /** 到期日期 **/
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date expireDate;
 
     /** 状态[0-已申请,1-通过,2-拒绝] **/
     private String status;
 
     /** 创建时间 **/
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /** 备注 **/
     private String remark;
+
+    /** 运费首件数 **/
+    private Long fareFirstNum;
+
+    /** 运费首费[单位：元] **/
+    private Double fareFirstPrice;
+
+    /** 运费续件数 **/
+    private Long fareNextNum;
+
+    /** 运费续费[单位：元] **/
+    private Double fareNextPrice;
 
 
     @Id
@@ -179,6 +186,42 @@ public class PhMerchant implements Serializable {
 
     public void setRemark(String remark) {
         this.remark = remark;
+    }
+
+    @Column(name = "fare_first_num", length = 11)
+    public Long getFareFirstNum() {
+        return fareFirstNum;
+    }
+
+    public void setFareFirstNum(Long fareFirstNum) {
+        this.fareFirstNum = fareFirstNum;
+    }
+
+    @Column(name = "fare_first_price", precision = 15, scale = 2)
+    public Double getFareFirstPrice() {
+        return fareFirstPrice;
+    }
+
+    public void setFareFirstPrice(Double fareFirstPrice) {
+        this.fareFirstPrice = fareFirstPrice;
+    }
+
+    @Column(name = "fare_next_num", length = 11)
+    public Long getFareNextNum() {
+        return fareNextNum;
+    }
+
+    public void setFareNextNum(Long fareNextNum) {
+        this.fareNextNum = fareNextNum;
+    }
+
+    @Column(name = "fare_next_price", precision = 15, scale = 2)
+    public Double getFareNextPrice() {
+        return fareNextPrice;
+    }
+
+    public void setFareNextPrice(Double fareNextPrice) {
+        this.fareNextPrice = fareNextPrice;
     }
 
 }
