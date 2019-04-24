6c7952e48aa8d255464dbc20cec3461b1b60df0c
XiaZhengXin <xzx@xiazhengxin.name>
2019-04-24 15:23:17 +0800

    APP用户列表。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/hades/service/PhUserInfoService.java
+++ src/main/java/cn/offway/hades/service/PhUserInfoService.java
@@ -1,6 +1,8 @@
 package cn.offway.hades.service;
 
 import cn.offway.hades.domain.PhUserInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * 用户信息Service接口
@@ -8,9 +10,11 @@ import cn.offway.hades.domain.PhUserInfo;
  * @author wn
  * @version $v: 1.0.0, $time:2019-04-04 15:18:00 Exp $
  */
-public interface PhUserInfoService{
+public interface PhUserInfoService {
 
-	PhUserInfo save(PhUserInfo phUserInfo);
-	
-	PhUserInfo findOne(Long id);
+    PhUserInfo save(PhUserInfo phUserInfo);
+
+    PhUserInfo findOne(Long id);
+
+    Page<PhUserInfo> list(String phone, String nickname, Pageable pageable);
 }
--- src/main/java/cn/offway/hades/service/impl/PhUserInfoServiceImpl.java
+++ src/main/java/cn/offway/hades/service/impl/PhUserInfoServiceImpl.java
@@ -1,13 +1,22 @@
 package cn.offway.hades.service.impl;
 
+import cn.offway.hades.domain.PhUserInfo;
+import cn.offway.hades.repository.PhUserInfoRepository;
+import cn.offway.hades.service.PhUserInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import cn.offway.hades.service.PhUserInfoService;
 
-import cn.offway.hades.domain.PhUserInfo;
-import cn.offway.hades.repository.PhUserInfoRepository;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,18 +28,37 @@ import cn.offway.hades.repository.PhUserInfoRepository;
 @Service
 public class PhUserInfoServiceImpl implements PhUserInfoService {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private PhUserInfoRepository phUserInfoRepository;
-	
-	@Override
-	public PhUserInfo save(PhUserInfo phUserInfo){
-		return phUserInfoRepository.save(phUserInfo);
-	}
-	
-	@Override
-	public PhUserInfo findOne(Long id){
-		return phUserInfoRepository.findOne(id);
-	}
+    @Autowired
+    private PhUserInfoRepository phUserInfoRepository;
+
+    @Override
+    public PhUserInfo save(PhUserInfo phUserInfo) {
+        return phUserInfoRepository.save(phUserInfo);
+    }
+
+    @Override
+    public Page<PhUserInfo> list(String phone, String nickname, Pageable pageable) {
+        return phUserInfoRepository.findAll(new Specification<PhUserInfo>() {
+            @Override
+            public Predicate toPredicate(Root<PhUserInfo> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> params = new ArrayList<Predicate>();
+                if (!"".equals(phone)) {
+                    params.add(criteriaBuilder.like(root.get("phone"), "%" + phone + "%"));
+                }
+                if (!"".equals(nickname)) {
+                    params.add(criteriaBuilder.like(root.get("nickname"), "%" + nickname + "%"));
+                }
+                Predicate[] predicates = new Predicate[params.size()];
+                criteriaQuery.where(params.toArray(predicates));
+                return null;
+            }
+        }, pageable);
+    }
+
+    @Override
+    public PhUserInfo findOne(Long id) {
+        return phUserInfoRepository.findOne(id);
+    }
 }
