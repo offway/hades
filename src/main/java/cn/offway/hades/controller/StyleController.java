8414772b43d628efb0194150730be8ce12ecd8fd
XiaZhengXin <xzx@xiazhengxin.name>
2019-06-25 17:32:55 +0800

    商品风格 模块 维护。。。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

---
+++ src/main/java/cn/offway/hades/controller/StyleController.java
@@ -0,0 +1,98 @@
+package cn.offway.hades.controller;
+
+import cn.offway.hades.domain.PhConfig;
+import cn.offway.hades.properties.QiniuProperties;
+import cn.offway.hades.service.PhConfigService;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping
+public class StyleController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private QiniuProperties qiniuProperties;
+    @Autowired
+    private PhConfigService configService;
+
+    @RequestMapping("/style.html")
+    public String index(ModelMap map) {
+        map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
+        return "style_index";
+    }
+
+    @RequestMapping("/style_list")
+    @ResponseBody
+    public Map<String, Object> list() {
+        Map<String, Object> list = new HashMap<>();
+        String jsonStr = configService.findContentByName("INDEX_STYLE");
+        JSONArray jsonArray = JSONArray.parseArray(jsonStr);
+        int i = 0;
+        List<Map> mapList = new ArrayList<>();
+        for (Map m : jsonArray.toJavaList(Map.class)) {
+            m.put("id", i);
+            i++;
+            mapList.add(m);
+        }
+        list.put("data", mapList);
+        return list;
+    }
+
+    @RequestMapping("/style_find")
+    @ResponseBody
+    public JSONObject fetch(Integer id) {
+        String jsonStr = configService.findContentByName("INDEX_STYLE");
+        JSONArray jsonArray = JSONArray.parseArray(jsonStr);
+        return jsonArray.getJSONObject(id);
+    }
+
+    @RequestMapping("/style_del")
+    @ResponseBody
+    public boolean delete(@RequestParam("ids[]") Long[] ids) {
+        String jsonStr = configService.findContentByName("INDEX_STYLE");
+        JSONArray jsonArray = JSONArray.parseArray(jsonStr);
+        List<Object> tmpList = new ArrayList<>();
+        for (Long id : ids) {
+            tmpList.add(jsonArray.get(id.intValue()));
+        }
+        for (Object o : tmpList) {
+            jsonArray.remove(o);
+        }
+        PhConfig config = configService.findOne("INDEX_STYLE");
+        config.setContent(jsonArray.toJSONString());
+        configService.save(config);
+        return true;
+    }
+
+    @RequestMapping("/style_save")
+    @ResponseBody
+    public boolean save(Integer id, String name, String image) {
+        String jsonStr = configService.findContentByName("INDEX_STYLE");
+        JSONArray jsonArray = JSONArray.parseArray(jsonStr);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("value", name);
+        jsonObject.put("image", image);
+        if (id == null) {
+            jsonArray.add(jsonObject);
+        } else {
+            jsonArray.set(id, jsonObject);
+        }
+        PhConfig config = configService.findOne("INDEX_STYLE");
+        config.setContent(jsonArray.toJSONString());
+        configService.save(config);
+        return true;
+    }
+}
