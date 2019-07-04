fd48288fb788444d86b851f743b8728a1d57a456
XiaZhengXin <xzx@xiazhengxin.name>
2019-07-04 14:04:14 +0800

    持久化数据处理..............
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -61,7 +61,29 @@ window.upload = function upload(param, token, file, next, error, complete, sizeL
             var newFileName = "video/wx/" + UUID.randomUUID() + postf;
             var observable = qiniu.upload(file, newFileName, token,
                 putExtra, config);
-            observable.subscribe(next, error, complete);
+            observable.subscribe(next, error, function (res, err) {
+                $.ajax({
+                    url: "https://api.qiniu.com/pfop/",
+                    type: 'post',
+                    headers: {
+                        "Authorization": "QBox " + token,
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Host": "api.qiniu.com"
+                    },
+                    data: {
+                        "bucket": "offwaypro",
+                        "key": newFileName,
+                        "fops": "xxxx",
+                        "pipeline": "xxxxxx"
+                    },
+                    success: function () {
+                        complete(res);
+                    },
+                    error: function () {
+                        complete('');
+                    }
+                });
+            });
         } else {
             getImgSize(file, function (w, h) {
                 var filename = file.name;
