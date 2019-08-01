2ea362129f79ee2ea4efd2097c2c8d7e2f0cd843
XiaZhengXin <xzx@xiazhengxin.name>
2019-08-01 17:25:23 +0800

    尝试集成spring-boot-admin 查看管理和监控Spring Boot应用程序,兼容性有问题，待解决。。。
    see: https://springboot.plus/guide/springbootadmin.html
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/hades/HadesApplication.java
+++ src/main/java/cn/offway/hades/HadesApplication.java
@@ -1,17 +1,18 @@
 package cn.offway.hades;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+//@EnableAdminServer
 @EnableScheduling
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.vinann","cn.offway"})
+@ComponentScan(basePackages = {"com.vinann", "cn.offway"})
 public class HadesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HadesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HadesApplication.class, args);
+    }
 }
