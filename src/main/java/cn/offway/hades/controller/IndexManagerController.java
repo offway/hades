d54e41d745f7203d146067240ca54319aeb2dd38
ShengjieLu <418511740@qq.com>
2019-08-21 14:50:15 +0800

    增加首页天天特价，新加INDEX_IMAGES_MINI字段用于小程序使用...

--- src/main/java/cn/offway/hades/controller/IndexManagerController.java
+++ src/main/java/cn/offway/hades/controller/IndexManagerController.java
@@ -24,38 +24,37 @@ public class IndexManagerController {
     private PhConfigService configService;
     private PhConfig phConfig;
     private JSONArray jsonArray;
+    private PhConfig phConfigMini;
+    private JSONArray jsonArrayMini;
 
     @Autowired
     public IndexManagerController(QiniuProperties qiniuProperties, PhConfigService configService) {
         this.qiniuProperties = qiniuProperties;
         this.configService = configService;
         String jsonStr = configService.findContentByName("INDEX_IMAGES");
+        String jsonStrMini = configService.findContentByName("INDEX_IMAGES_MINI");
         if (jsonStr != null && !"".equals(jsonStr)) {
             jsonArray = JSON.parseArray(jsonStr);
         }
+        if (jsonStrMini != null&& !"".equals(jsonStrMini)){
+            jsonArrayMini =JSON.parseArray(jsonStrMini);
+        }
         phConfig = configService.findOne("INDEX_IMAGES");
+        phConfigMini = configService.findOne("INDEX_IMAGES_MINI");
     }
 
-    private String getImgFromJSON(String num) {
-        if (jsonArray != null) {
-            for (Object o : jsonArray) {
+    private String getImgFromJSON(String num,String who) {
+        JSONArray jsonArr = null;
+        if ("xcx".equals(who)){
+            jsonArr = jsonArrayMini;
+        }else if ("app".equals(who)){
+            jsonArr = jsonArray;
+        }
+        if (jsonArr != null) {
+            for (Object o : jsonArr) {
                 JSONObject obj = (JSONObject) o;
                 if (num.equals(obj.getString("type"))) {
                     return obj.getString("image");
-                }
-            }
-            return "";
-        } else {
-            return "";
-        }
-    }
-
-    private String getImgXcxFromJSON(String num) {
-        if (jsonArray != null) {
-            for (Object o : jsonArray) {
-                JSONObject obj = (JSONObject) o;
-                if (num.equals(obj.getString("type"))) {
-                    return obj.getString("imageXcx");
                 }
             }
             return "";
@@ -73,7 +72,6 @@ public class IndexManagerController {
             map.put("type", id);
             map.put("value", value);
             map.put("image", logo);
-            map.put("imageXcx",logoXcx);
             list.add(map);
             jsonArray = new JSONArray(list);
         } else {
@@ -91,7 +89,6 @@ public class IndexManagerController {
             jsonObject.put("type", id);
             jsonObject.put("value", value);
             jsonObject.put("image", logo);
-            jsonObject.put("imageXcx",logoXcx);
             jsonArray.add(jsonObject);
         }
         if (phConfig == null) {
@@ -101,14 +98,47 @@ public class IndexManagerController {
         }
         phConfig.setContent(jsonArray.toJSONString());
         configService.save(phConfig);
+        if (!"".equals(logoXcx)){
+            if (jsonArrayMini ==null){
+                List<Object> list = new ArrayList<>();
+                Map<String,String> map = new HashMap<>();
+                map.put("type", id);
+                map.put("value", value);
+                map.put("image", logoXcx);
+                list.add(map);
+            }else {
+                Object it = null;
+                for (Object o : jsonArrayMini) {
+                    JSONObject obj = (JSONObject) o;
+                    if (id.equals(obj.getString("type"))) {
+                        it = obj;
+                    }
+                }
+                if (it != null) {
+                    jsonArrayMini.remove(it);
+                }
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("type", id);
+                jsonObject.put("value", value);
+                jsonObject.put("image", logoXcx);
+                jsonArrayMini.add(jsonObject);
+            }
+            if (phConfigMini ==null){
+                phConfigMini = new PhConfig();
+                phConfigMini.setName("INDEX_IMAGES_MINI");
+                phConfigMini.setCreateTime(new Date());
+            }
+            phConfigMini.setContent(jsonArrayMini.toJSONString());
+            configService.save(phConfigMini);
+        }
         return true;
     }
 
     @RequestMapping("/index_zgll.html")
     public String zgll(ModelMap map) {
         map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
-        map.addAttribute("image", getImgFromJSON("0"));
-        map.addAttribute("imageXcx", getImgXcxFromJSON("0"));
+        map.addAttribute("image", getImgFromJSON("0","app"));
+        map.addAttribute("imageXcx", getImgFromJSON("0","xcx"));
         map.addAttribute("type", "0");
         map.addAttribute("value", "中国力量");
         return "index_common";
@@ -117,8 +147,8 @@ public class IndexManagerController {
     @RequestMapping("/index_gjcl.html")
     public String gjcl(ModelMap map) {
         map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
-        map.addAttribute("image", getImgFromJSON("1"));
-        map.addAttribute("imageXcx", getImgXcxFromJSON("1"));
+        map.addAttribute("image", getImgFromJSON("1","app"));
+        map.addAttribute("imageXcx", getImgFromJSON("1","xcx"));
         map.addAttribute("type", "1");
         map.addAttribute("value", "高街潮流");
         return "index_common";
@@ -127,8 +157,8 @@ public class IndexManagerController {
     @RequestMapping("/index_msd.html")
     public String msd(ModelMap map) {
         map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
-        map.addAttribute("image", getImgFromJSON("2"));
-        map.addAttribute("imageXcx", getImgXcxFromJSON("2"));
+        map.addAttribute("image", getImgFromJSON("2","app"));
+        map.addAttribute("imageXcx", getImgFromJSON("2","xcx"));
         map.addAttribute("type", "2");
         map.addAttribute("value", "买手店");
         return "index_common";
@@ -137,8 +167,8 @@ public class IndexManagerController {
     @RequestMapping("/index_xlzq.html")
     public String xlzq(ModelMap map) {
         map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
-        map.addAttribute("image", getImgFromJSON("3"));
-        map.addAttribute("imageXcx", getImgXcxFromJSON("3"));
+        map.addAttribute("image", getImgFromJSON("3","app"));
+        map.addAttribute("imageXcx", getImgFromJSON("3","xcx"));
         map.addAttribute("type", "3");
         map.addAttribute("value", "限量专区");
         return "index_common";
@@ -147,10 +177,20 @@ public class IndexManagerController {
     @RequestMapping("/index_mfs.html")
     public String mfs(ModelMap map) {
         map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
-        map.addAttribute("image", getImgFromJSON("4"));
-        map.addAttribute("imageXcz", getImgXcxFromJSON("4"));
+        map.addAttribute("image", getImgFromJSON("4","app"));
+        map.addAttribute("imageXcx", getImgFromJSON("4","xcx"));
         map.addAttribute("type", "4");
         map.addAttribute("value", "免费送");
+        return "index_common";
+    }
+
+    @RequestMapping("/index_tj.html")
+    public String tj(ModelMap map) {
+        map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
+        map.addAttribute("image", getImgFromJSON("5","app"));
+        map.addAttribute("imageXcx", getImgFromJSON("5","xcx"));
+        map.addAttribute("type", "5");
+        map.addAttribute("value", "天天特价");
         return "index_common";
     }
 
