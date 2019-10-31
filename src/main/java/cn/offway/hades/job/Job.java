1fec12aa7c42d3a75389bc77e7c2eaa3949e0fe9
XiaZhengXin <xzx@xiazhengxin.name>
2019-10-31 18:45:29 +0800

    创建小时间隔的定时器检查品牌下是否有至少一件上架商品。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

---
+++ src/main/java/cn/offway/hades/job/Job.java
@@ -0,0 +1,37 @@
+package cn.offway.hades.job;
+
+import cn.offway.hades.domain.PhMerchantBrand;
+import cn.offway.hades.repository.PhMerchantBrandRepository;
+import cn.offway.hades.service.PhGoodsService;
+import cn.offway.hades.service.PhMerchantBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Job {
+    @Autowired
+    private PhMerchantBrandService merchantBrandService;
+    @Autowired
+    private PhMerchantBrandRepository merchantBrandRepository;
+    @Autowired
+    private PhGoodsService goodsService;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void disableEmptyBrand() {
+        //SELECT count(b.id) as ct,a.brand_id,a.brand_name,a.merchant_id,a.merchant_name,a.id as pk,b.id as fk FROM phweb.ph_merchant_brand a left join phweb.ph_goods b on a.brand_id = b.brand_id group by a.brand_id ,a.merchant_id
+        List<Object[]> list = merchantBrandRepository.checkEmptyBrand();
+        for (Object[] item : list) {
+            long count = Long.valueOf(String.valueOf(item[0]));
+            if (count == 0) {
+                long pk = Long.valueOf(String.valueOf(item[5]));
+                PhMerchantBrand merchantBrand = merchantBrandService.findOne(pk);
+                if (merchantBrand != null) {
+                    //
+                }
+            }
+        }
+    }
+}
