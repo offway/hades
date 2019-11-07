512ce1b05d390b1c86048892aeb19c92fa0ddf45
XiaZhengXin <xzx@xiazhengxin.name>
2019-11-07 16:39:39 +0800

    定时器检查品牌是否需要下架商品，筛选的品牌不局限于商家下属的品牌，而是全平台的品牌库中的品牌。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/hades/job/Job.java
+++ src/main/java/cn/offway/hades/job/Job.java
@@ -1,12 +1,8 @@
 package cn.offway.hades.job;
 
 import cn.offway.hades.domain.PhBrand;
-import cn.offway.hades.domain.PhGoods;
-import cn.offway.hades.domain.PhMerchantBrand;
 import cn.offway.hades.repository.PhMerchantBrandRepository;
 import cn.offway.hades.service.PhBrandService;
-import cn.offway.hades.service.PhGoodsService;
-import cn.offway.hades.service.PhMerchantBrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,28 +12,21 @@ import java.util.List;
 @Component
 public class Job {
     @Autowired
-    private PhMerchantBrandService merchantBrandService;
-    @Autowired
     private PhMerchantBrandRepository merchantBrandRepository;
-    @Autowired
-    private PhGoodsService goodsService;
     @Autowired
     private PhBrandService brandService;
 
     @Scheduled(cron = "0 * * * * *")
     public void disableEmptyBrand() {
-        //SELECT count(b.id) as ct,a.brand_id,a.brand_name,a.merchant_id,a.merchant_name,a.id as pk,b.id as fk FROM phweb.ph_merchant_brand a left join phweb.ph_goods b on a.brand_id = b.brand_id group by a.brand_id ,a.merchant_id
-        List<Object[]> list = merchantBrandRepository.checkEmptyBrand();
+        List<Object[]> list = merchantBrandRepository.checkAllEmptyBrand();
         for (Object[] item : list) {
             long count = Long.valueOf(String.valueOf(item[0]));
             if (count == 0) {
-                long pk = Long.valueOf(String.valueOf(item[5]));
-                PhMerchantBrand merchantBrand = merchantBrandService.findOne(pk);
-                if (merchantBrand != null) {
-                    PhBrand brand = brandService.findOne(merchantBrand.getBrandId());
+                long pk = Long.valueOf(String.valueOf(item[1]));
+                PhBrand brand = brandService.findOne(pk);
+                if (brand != null) {
                     brand.setStatus("0");
                     brandService.save(brand);
-                    //
                 }
             }
         }
