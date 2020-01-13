35826bcb5e13593ca0e663d89c0ff5762f5187e8
XiaZhengXin <xzx@xiazhengxin.name>
2020-01-13 13:55:26 +0800

    渠道财务报表 接口基础代码提交。。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/hades/controller/ChannelUserController.java
+++ src/main/java/cn/offway/hades/controller/ChannelUserController.java
@@ -1,25 +1,29 @@
 package cn.offway.hades.controller;
 
+import cn.offway.hades.domain.PhAdmin;
 import cn.offway.hades.domain.PhChannelUser;
-import cn.offway.hades.domain.PhGoodsType;
 import cn.offway.hades.domain.PhUserInfo;
 import cn.offway.hades.service.PhChannelUserService;
+import cn.offway.hades.service.PhRoleadminService;
 import cn.offway.hades.service.PhUserInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigInteger;
+import java.util.*;
 
 @Controller
 @RequestMapping
@@ -29,6 +33,8 @@ public class ChannelUserController {
     private PhChannelUserService phChannelUserService;
     @Autowired
     private PhUserInfoService phUserInfoService;
+    @Autowired
+    private PhRoleadminService roleadminService;
 
 
     @RequestMapping("/channelhtml.html")
@@ -58,17 +64,67 @@ public class ChannelUserController {
         return map;
     }
 
+    @RequestMapping("/channel_settle_index.html")
+    public String listIndex(ModelMap map, @AuthenticationPrincipal PhAdmin admin) {
+        List<Long> roles = roleadminService.findRoleIdByAdminId(admin.getId());
+        if (roles.contains(BigInteger.valueOf(1L))) {
+            List<PhChannelUser> list = phChannelUserService.listAll();
+            PhChannelUser all = new PhChannelUser();
+            all.setChannelName("全部");
+            all.setChannel("");
+            list.add(all);
+            map.addAttribute("channels", list);
+        } else {
+            map.addAttribute("channels", phChannelUserService.findByUserId(admin.getId()));
+        }
+        return "channel_settle_index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/channel_list")
+    public Map<String, Object> list(HttpServletRequest request, String channel, int sEcho, int iDisplayStart, int iDisplayLength) {
+        String sortCol = request.getParameter("iSortCol_0");
+        String sortName = request.getParameter("mDataProp_" + sortCol);
+        String sortDir = request.getParameter("sSortDir_0");
+        Sort sort = new Sort("id");
+        PageRequest pr = new PageRequest(iDisplayStart == 0 ? 0 : iDisplayStart / iDisplayLength, iDisplayLength < 0 ? 9999999 : iDisplayLength, sort);
+        Page<PhChannelUser> pages = phChannelUserService.list(channel, pr);
+        ObjectMapper mapper = new ObjectMapper();
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (PhChannelUser channelUser : pages.getContent()) {
+            Map<String, Object> m = mapper.convertValue(channelUser, Map.class);
+            m.put("userCount", phChannelUserService.statUsers(channelUser.getChannel()));
+            Object[] l = phChannelUserService.statOrder(channelUser.getChannel());
+            m.put("totalPrice", l[0]);
+            m.put("totalCount", l[1]);
+            if (l[0] == null || StringUtils.isNumeric(String.valueOf(l[0]))) {
+                m.put("backAmount", 0);
+            } else {
+                m.put("backAmount", Double.valueOf(String.valueOf(l[0])) * channelUser.getProportion());
+            }
+            list.add(m);
+        }
+        // 为操作次数加1，必须这样做
+        int initEcho = sEcho + 1;
+        Map<String, Object> map = new HashMap<>();
+        map.put("sEcho", initEcho);
+        map.put("iTotalRecords", pages.getTotalElements());//数据总条数
+        map.put("iTotalDisplayRecords", pages.getTotalElements());//显示的条数
+        map.put("aData", list);//数据集合
+        return map;
+    }
+
     @ResponseBody
     @RequestMapping("/channel_user_save")
     public boolean save(PhChannelUser phChannelUser) {
-        if (null != phChannelUser){
+        if (null != phChannelUser) {
             PhChannelUser channelUser = phChannelUserService.findByUserId(phChannelUser.getUserId());
-            if (channelUser != null){
+            if (channelUser != null) {
                 return false;
             }
             PhUserInfo userInfo = phUserInfoService.findOne(phChannelUser.getUserId());
-            if (null != userInfo){
-                if(userInfo.getHeadimgurl() != null){
+            if (null != userInfo) {
+                if (userInfo.getHeadimgurl() != null) {
                     phChannelUser.setUserHeadimgurl(userInfo.getHeadimgurl());
                 }
                 phChannelUser.setUserPhone(userInfo.getPhone());
@@ -91,7 +147,7 @@ public class ChannelUserController {
 
     @ResponseBody
     @RequestMapping("/channel_user_del")
-    public boolean del(@RequestParam("ids[]")Long[] ids) {
+    public boolean del(@RequestParam("ids[]") Long[] ids) {
         for (Long aLong : ids) {
             phChannelUserService.delete(aLong);
         }
--- src/main/java/cn/offway/hades/service/PhChannelUserService.java
+++ src/main/java/cn/offway/hades/service/PhChannelUserService.java
@@ -1,11 +1,11 @@
 package cn.offway.hades.service;
 
 
-import java.util.List;
-
 import cn.offway.hades.domain.PhChannelUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 /**
  * 用户推广渠道表Service接口
@@ -13,10 +13,10 @@ import org.springframework.data.domain.Pageable;
  * @author tbw
  * @version $v: 1.0.0, $time:2020-01-10 11:33:13 Exp $
  */
-public interface PhChannelUserService{
+public interface PhChannelUserService {
 
     PhChannelUser save(PhChannelUser phChannelUser);
-	
+
     PhChannelUser findOne(Long id);
 
     void delete(Long id);
@@ -26,4 +26,12 @@ public interface PhChannelUserService{
     PhChannelUser findByUserId(Long id);
 
     Page<PhChannelUser> list(String channelName, String channel, String userId, String userPhone, Pageable pageable);
+
+    Page<PhChannelUser> list(String channel, Pageable pageable);
+
+    Long statUsers(String channel);
+
+    Object[] statOrder(String channel);
+
+    List<PhChannelUser> listAll();
 }
