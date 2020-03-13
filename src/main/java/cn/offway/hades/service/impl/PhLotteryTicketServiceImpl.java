15bf702c8a1f3f5a015c7db47717d267b2abffab
XiaZhengXin <xzx@xiazhengxin.name>
2020-03-13 14:48:11 +0800

    后台开奖微信模板消息改为使用 订阅消息。。。。。。 增加微信工具类。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/hades/service/impl/PhLotteryTicketServiceImpl.java
+++ src/main/java/cn/offway/hades/service/impl/PhLotteryTicketServiceImpl.java
@@ -1,18 +1,12 @@
 package cn.offway.hades.service.impl;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaBuilder.In;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
+import cn.offway.hades.domain.PhLotteryTicket;
+import cn.offway.hades.domain.PhProductInfo;
+import cn.offway.hades.dto.VTicketCount;
+import cn.offway.hades.repository.PhLotteryTicketRepository;
+import cn.offway.hades.service.PhLotteryTicketService;
+import cn.offway.hades.service.PhProductInfoService;
+import cn.offway.hades.utils.WXUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,18 +18,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-
-import cn.offway.hades.domain.PhLotteryTicket;
-import cn.offway.hades.domain.PhProductInfo;
-import cn.offway.hades.dto.Template;
-import cn.offway.hades.dto.TemplateParam;
-import cn.offway.hades.dto.VTicketCount;
-import cn.offway.hades.repository.PhLotteryTicketRepository;
-import cn.offway.hades.service.PhLotteryTicketService;
-import cn.offway.hades.service.PhProductInfoService;
-import cn.offway.hades.utils.HttpClientUtil;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaBuilder.In;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 
@@ -203,33 +197,15 @@ public class PhLotteryTicketServiceImpl implements PhLotteryTicketService {
 	}
 
 	@Override
-	public void notice(String token, PhProductInfo phProductInfo) throws Exception {
+	public void notice(String token, PhProductInfo phProductInfo) {
 		List<Object> datas = phLotteryTicketRepository.findNoticeData(phProductInfo.getId());
 		for (Object obj : datas) {
-			Object[] data = (Object[])obj;
+			Object[] data = (Object[]) obj;
 			String openid = String.valueOf(data[0]);
-			String formid = String.valueOf(data[1]);
-			
-			// 模块消息配置
-			Template tem = new Template();
-			tem.setTemplateId("WTNwoOQEHWerZGTi9W-yLOjx4aavpxsArHtZoWBeLSQ");
-			tem.setFormId(formid);
-			tem.setTopColor("#00DD00");
-			tem.setToUser(openid);
-			tem.setPage("pages/promo_reg/promo_reg.js?sharePath=true&id="+phProductInfo.getId());
-			
-			List<TemplateParam> paras = new ArrayList<TemplateParam>();
-			paras.add(new TemplateParam("keyword1", "开奖通知", "#0044BB"));
-			paras.add(new TemplateParam("keyword2", phProductInfo.getName(), "#0044BB"));
-			paras.add(new TemplateParam("keyword3", "活动已开奖，幸运儿是你吗？点击查看！", "#0044BB"));
-			
-			tem.setEmphasis_keyword("keyword1.DATA");
-			
-			tem.setTemplateParamList(paras);
-			
-			// 推送模版消息
-			sendTemplateMsg(tem, token);
-			
+			// 订阅消息包装
+			Map<String, Object> args = WXUtil.buildMsg(openid, "pages/promo_reg/promo_reg.js?sharePath=true&id=" + phProductInfo.getId(), "开奖通知", phProductInfo.getName(), "活动已开奖，幸运儿是你吗？点击查看！");
+			// 推送订阅消息
+			WXUtil.sendSubscribeMsg(logger, args, token);
 		}
 	}
 
@@ -238,38 +214,6 @@ public class PhLotteryTicketServiceImpl implements PhLotteryTicketService {
 	 */
 	@Override
 	public String getToken() {
-		String requestUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx12d022a9493f1b26&secret=52ba3a89ae58aa6a2294806d516d6107";
-		String result = HttpClientUtil.post(requestUrl, "");
-		JSONObject jsonObject = JSON.parseObject(result);
-		if (jsonObject != null) {
-			String access_token = jsonObject.getString("access_token");
-			return access_token;
-		} else {
-			return "";
-		}
+		return WXUtil.getToken();
 	}
-
-	public void sendTemplateMsg(Template template, String token) {
-		String requestUrl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=ACCESS_TOKEN";
-		requestUrl = requestUrl.replace("ACCESS_TOKEN", token);
-		String jsonString = template.toJSON();
-
-		String result = HttpClientUtil.post(requestUrl, jsonString);
-		JSONObject jsonResult = JSON.parseObject(result);
-		if (jsonResult != null) {
-			int errorCode = jsonResult.getIntValue("errcode");
-			String errorMessage = jsonResult.getString("errmsg");
-			if (errorCode == 0) {
-				logger.info("模板消息发送成功:" + jsonResult);
-			} else {
-				logger.info("模板消息发送失败:" + errorCode + "," + errorMessage);
-			}
-		} else {
-			logger.info("模板消息发送失败:" + jsonResult);
-		}
-	}
-	
-	
-
-	
 }
